311e4e29c8ba9b16bdd226c87cd44e9d49d8958c
onedi <sevencrime77@gmail.com>
2019-12-15 21:20:30 +0800

    m3u8基本完成

--- 爬虫/爬m3u8/get_m3u8_viedo.py
+++ 爬虫/爬m3u8/get_m3u8_viedo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import multiprocessing
+# from pathos.multiprocessing import ProcessingPool as Pool
+from multiprocessing import Pool
 import requests
 from bs4 import BeautifulSoup
 import os
@@ -13,13 +14,13 @@ from selenium import webdriver
 from browsermobproxy import Server
 from selenium.webdriver.chrome.options import Options
 
+
 class ViedeoCrawler():
 
     MYSQL = False
 
-    def __init__(self, url, func):
+    def __init__(self, url=""):
         self.url = url
-        self.code = code
         self.down_path = r"D:\\private\\爬虫"
         self.final_path = r"D:\\private"
         try:
@@ -27,17 +28,14 @@ class ViedeoCrawler():
         except:
             self.name = "uncensord"
         self.headers = {
-            'Connection': 'Keep-Alive',
-            # 'Connection': 'close',
+            # 'Connection': 'Keep-Alive',
+            'Connection': 'close',
             'Accept': 'text/html, application/xhtml+xml, */*',
             'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
             'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
         }
+        requests.adapters.DEFAULT_RETRIES = 5
 
-        # 连接数据库, 不存在则自动创建
-        self.conn = sqlite3.connect("viedo_ts.db")
-        # 创建游标cursor
-        self.cursor = self.conn.cursor()
 
     def get_ip_list(self):
         print("正在获取代理列表...")
@@ -64,15 +62,15 @@ class ViedeoCrawler():
         print("代理设置成功.")
         return proxies
 
-    def get_uri_from_m3u8(self):
+    def get_uri_from_m3u8(self, cursor):
         print("正在解析真实下载地址...")
         torrentNamelist = []
 
         # 遍历torrent 未保存成文件的数据
-        for data in self.cursor.execute("SELECT * FROM torrent WHERE isGet=0"):
+        for data in cursor.execute("SELECT * FROM torrent WHERE isGet != 1"):
             print(data)
             torrentName = ''.join(re.findall(r'[A-Za-z]+-\d+', data[1])).replace('-', '_')
-            self.cursor.execute("CREATE TABLE IF NOT EXISTS {torrentName} (id INTEGER PRIMARY KEY AUTOINCREMENT, Url VARCHAR(255) NOT NULL, isGet INT NOT NULL);".format(torrentName=torrentName))
+            cursor.execute("CREATE TABLE IF NOT EXISTS {torrentName} (id INTEGER PRIMARY KEY AUTOINCREMENT, Url VARCHAR(255) NOT NULL, isGet INT NOT NULL);".format(torrentName=torrentName))
 
             with open(torrentName+'.m3u8', 'wb') as file:
                 file.write(requests.get("".join(data[2])).content)
@@ -89,22 +87,23 @@ class ViedeoCrawler():
                 tsurl = path + key.uri
                 # 插入记录
                 # self.cursor.execute("INSERT OR IGNORE INTO temp (id, Url, isGet) VALUES (?, ?, ?);", (None, tsurl, 0))
-                self.cursor.execute("INSERT OR IGNORE INTO {torrentName} (id, Url, isGet) VALUES (?, ?, ?);".format(torrentName=torrentName), (None, tsurl, 0))
+                cursor.execute("INSERT OR IGNORE INTO {torrentName} (id, Url, isGet) VALUES (?, ?, ?);".format(torrentName=torrentName), (None, tsurl, 0))
 
             # 改变torrent表的isGet, 表示已爬取过
-            self.cursor.execute("UPDATE torrent set isGet = 1 where id={}".format(data[0]))
+            cursor.execute("UPDATE torrent set isGet = 1 where id={}".format(data[0]))
             torrentNamelist.append(torrentName)
 
+            # 删除m3u8文件
+            try:
+                os.remove(torrentName +'.m3u8')
+            except:
+                pass
+
         # 提交事务
-        self.conn.commit()
-        # 删除m3u8文件
-        try:
-            os.remove(torrentName +'.m3u8')
-        except:
-            pass
+        conn.commit()
 
 
-    def get_viedo_downURL(self):
+    def get_viedo_downURL(self, cursor):
         print("开始请求资源网站")
         # 建立browsermobproxy服务, 需指定browsermob-proxy, 类似chromedriver
         server = Server("D:/下载/browsermob-proxy-2.1.4/bin/browsermob-proxy.bat")
@@ -115,7 +114,7 @@ class ViedeoCrawler():
         # 为chrome启动时设置代理
         chrome_options.add_argument('--proxy-server={0}'.format(proxy.proxy))
         # 静默模式, 不显示浏览器
-        # chrome_options.add_argument('headless')
+        chrome_options.add_argument('headless')
 
         driver = webdriver.Chrome(
             executable_path='C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe',
@@ -127,20 +126,23 @@ class ViedeoCrawler():
         proxy.new_har(ref="HAR啦", options={'captureHeaders': True, 'captureContent': True}, title="标题")
         driver.get(self.url)
         title = driver.title
+        torrentName = ''.join(re.findall(r'[A-Za-z]+-\d+', title)).replace('-', '_')
         # 获取HAR
         result = proxy.har
         print(result)
+
         # m3u8UrlSet = set()
         # 把爬取的链接和标题存入数据库
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS torrent(id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(255) NOT NULL, fileUrl VARCHAR(255) NOT NULL, isGet INT NOT NULL);")
+        cursor.execute("CREATE TABLE IF NOT EXISTS torrent(id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR(255) NOT NULL, fileUrl VARCHAR(255) NOT NULL, isGet INT NOT NULL);")
         for entry in result['log']['entries']:
             _url = entry['request']['url']
             # 根据URL找到数据接口
             if "m3u8" in _url:
                 print("找到M3U8文件了")
                 print(_url)
-                self.cursor.execute("INSERT INTO torrent(id, title, fileUrl, isGet) VALUES (?, ?, ?, ?);", (None, title, _url, 0))
+                cursor.execute("INSERT INTO torrent(id, title, fileUrl, isGet) VALUES (?, ?, ?, ?);", (None, title, _url, 0))
                 # m3u8UrlSet.add(_url)
+                return torrentName
 
             # 判断响应是否存在error
             if "_error" in entry['response'].keys():
@@ -152,74 +154,52 @@ class ViedeoCrawler():
         server.stop()
         driver.quit()
 
-        torrentName = ''.join(re.findall(r'[A-Za-z]+-\d+', title)).replace('-', '_')
+        return torrentName
+
+
+    def run(self, cursor):
+        print("开始执行程序")
+        os.chdir(self.down_path)
+
+        torrentName = ''
+        # # 开始请求资源网站
+        # torrentName = self.get_viedo_downURL(cursor)
+        # # 解析M3U8文件
+        # self.get_uri_from_m3u8(cursor)
+
+        self.ip_list = self.get_ip_list()
+        self.proxies = self.get_random_ip(self.ip_list)
 
         return torrentName
 
-    def get_viedo(self, key, ip, proxies):
-        import pdb; pdb.set_trace()
+
+    def generateFile(self, key):
 
         if key[0] % 20 == 0:
             print("休眠10s")
             time.sleep(10)
-        if key[0] % 50 == 0:
+        if key[0] % 40 == 0:
             print("更换代理IP")
-            proxies = self.get_random_ip(ip)
+            self.proxies = self.get_random_ip(self.ip_list)
         try:
-            # 存入数据库
-            resp = requests.get(key[1], headers=self.headers, proxies=proxies)
+            resp = requests.get(key[1], headers=self.headers, proxies=self.proxies)
         except Exception as e:
-            print(e, "222222222")
-            return
+            print(e, "文件 {} 没有下载".format(key[0]))
+            return True
 
-        name = ('clip_{}.ts'.format(key[0]))
+        name = 'clip_{}.ts'.format(str(key[0]).zfill(6))
         print('开始下载{}'.format(name))
         with open(name, 'wb') as f:
             f.write(resp.content)
             # f.flush()
             # os.fsync(f)
 
-
-    def run(self):
-        print("Start!")
-        start_time = time.time()
-        os.chdir(self.down_path)
-
-        fileUrlSet = ""
-
-        ip_list = self.get_ip_list()
-        proxies = self.get_random_ip(ip_list)
-
-        # isFile = True
-        # for i in os.listdir(self.down_path):
-        #     if os.path.splitext(i)[1].find("m3u8") != -1:
-        #         isFile = False
-        #
-        # if isFile:
-        # torrentName = self.get_viedo_downURL()
-        # self.get_uri_from_m3u8()
-        # i = 1   # count
-        torrentName = "SDMU_638"
-        # datalist = self.cursor.execute('SELECT * FROM {}'.format(torrentName))
-        datalist = self.cursor.execute('SELECT * FROM {}'.format(torrentName))
-        results = []
-        p = multiprocessing.Pool(4)
-        for key in datalist:
-            print("开始进入线程池_{}".format(key))
-            # p.apply_async(self.get_viedo, args=(key, ip_list, proxies, ))
-            results.append(p.apply_async(self.get_viedo, args=(key, ip_list, proxies, )))
-
-        for res in results:
-            print(res.get())
-
-        p.close()
-        p.join()
-        self.cursor.close()
-        self.conn.close()
-
+    def mergeFile(self, torrentName, start_time= 0):
         print("下载完成！总共耗时 %d s" % (time.time()-start_time))
+        import pdb; pdb.set_trace()
+        os.chdir(self.down_path)
         print("接下来进行合并……")
-        os.system('copy/b %s\\*.ts %s\\%s.ts' % (self.down_path,self.final_path, self.name))
+        os.system('copy/b %s\\*.ts %s\\%s.ts' % (self.down_path,self.final_path, torrentName))
         print("合并完成，请您欣赏！")
         y = input("请检查文件完整性，并确认是否要删除碎片源文件？(y/n)")
         if y=='y':
@@ -232,12 +212,25 @@ class ViedeoCrawler():
             print("不删除，程序结束。")
 
 
-def long_time_task_wrapper(cls_instance, i):
-    return cls_instance.get_viedo(i)
-
-
 if __name__=='__main__':
     url = r"https://www.kpl052.com/Watch-online/146998-1-1.html"
-    code = "hello12200"
     crawler = ViedeoCrawler(url)
-    crawler.run()
+    conn = sqlite3.connect("viedo_ts.db")
+    cursor = conn.cursor()
+
+    torrentName = crawler.run(cursor)
+    torrentName = 'SDMU_638'
+
+    p = Pool(32)
+    start = time.time()
+    for key in cursor.execute("SELECT * FROM {}".format(torrentName)):
+        p.apply_async(crawler.generateFile, args=(key,))
+
+    p.close()
+    p.join()
+    # 改变torrent表的isGet, 表示已爬取过
+    cursor.close()
+    conn.close()
+    crawler.mergeFile(torrentName, start)
+
+
